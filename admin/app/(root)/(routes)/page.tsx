76f5852a3003f2bd63d452abfaa5c46e42a76a39
Hammad-K9 <37565310+Hammad-K9@users.noreply.github.com>
2024-05-17 11:25:08 -0400

    Add dashboard for store

--- admin/app/(root)/(routes)/page.tsx
+++ admin/app/(root)/(routes)/page.tsx
@@ -17,12 +17,9 @@ const SetupPage = () => {
     if (!isOpen) {
       onOpen();
     }
-  }, [isOpen, onOpen])
-  return (
-    <div className="p-4">
-      Root Page
-    </div>
-  );
+  }, [isOpen, onOpen]);
+  
+  return null;
 }
 
 export default SetupPage;
